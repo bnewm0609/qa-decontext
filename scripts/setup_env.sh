3a61a36e86af7dc00c12fcd8d8910c40723dc30b
Benjamin Newman <nlb9991@gmail.com>
2023-07-07 10:37:58 -0700

    Add documents for retrieval

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,4 +1,5 @@
 pip install -r requirements.txt
 python -c "import nltk; nltk.download('stopwords')"
 tar -xvf data/full_texts.tar.gz
+tar -xvf data/emnlp23/full_texts.tar.gz
 # python -m spacy download en_core_web_sm
